068688f20172116af720219b5b22c7c35abece59
LouisOrellana <orellana.morales.louis@gmail.com>
2020-07-20 16:18:05 -0700

    Create main.py
    Created main program and base of the game

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+
+blockSize, mapSize, margin = 15, 600, 30
+x, y = margin, margin
+
+black, white, snakeColor = (0,0,0), (255,255,255), (255,0,255)
+
+pygame.init()
+win = pygame.display.set_mode((mapSize,mapSize))
+pygame.display.set_caption("Snake Game")
+
+run = True
+
+def drawGrid(win, color):
+    gridlineCount = (mapSize-(2*margin))//blockSize + 1
+    for i in range(gridlineCount):
+        pygame.draw.line(win, color, (margin + i*blockSize, margin), (margin + i*blockSize, mapSize-margin), 1)
+        pygame.draw.line(win, color, (margin, margin + i*blockSize), (mapSize-margin, margin + i*blockSize), 1)
+        
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    
+    win.fill(black)
+    
+    pygame.draw.rect(win, snakeColor, (x, y, blockSize, blockSize))   
+    drawGrid(win, white)
+    
+    pygame.display.update()
+    pygame.time.delay(70)
+
+    
+pygame.quit()
