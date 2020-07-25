fe5514b275bb11e6d9f3f97b5aac9383e4e650db
LouisOrellana <orellana.morales.louis@gmail.com>
2020-07-25 00:08:19 -0700

    Added Box Class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 
-blockSize, mapSize, margin = 15, 600, 30
-x, y = margin, margin
+blockSize, mapSize, margin = 20, 600, 30
 
 black, white, snakeColor = (0,0,0), (255,255,255), (255,0,255)
 
@@ -9,38 +8,61 @@ pygame.init()
 win = pygame.display.set_mode((mapSize,mapSize))
 pygame.display.set_caption("Snake Game")
 
-run = True
 
+
+class Box:
+    def __init__(self, pos=[0, 0], color=snakeColor):
+        self.pos   = pos
+        self.color = color
+        
+        
+    def draw(self):
+        xPos = margin + (self.pos[0] * blockSize)
+        yPos = margin + (self.pos[1] * blockSize)
+        pygame.draw.rect(win, self.color, (xPos, yPos, blockSize, blockSize))
+    
+    def move(self, deltaX, deltaY):
+        self.pos[0]+=deltaX
+        self.pos[1]+=deltaY        
+        
+ 
+
+
+       
 def drawGrid(win, color):
     gridlineCount = (mapSize-(2*margin))//blockSize + 1
     for i in range(gridlineCount):
         pygame.draw.line(win, color, (margin + i*blockSize, margin), (margin + i*blockSize, mapSize-margin), 1)
         pygame.draw.line(win, color, (margin, margin + i*blockSize), (mapSize-margin, margin + i*blockSize), 1)
+
+
+
         
 pressedKeys = []
+snake = Box()
+run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
             pressedKeys.append(event.key)
-        if event.type == pygame.KEYUP:
+        if event.type == pygame.KEYUP:  
             pressedKeys.remove(event.key)
     if pressedKeys:
         if pressedKeys[-1] == pygame.K_LEFT:
-            x-=blockSize
+            snake.move(-1, 0)
         elif pressedKeys[-1] == pygame.K_RIGHT:
-            x+=blockSize
+            snake.move(1, 0)
         elif pressedKeys[-1] == pygame.K_UP:
-            y-=blockSize
+            snake.move(0, -1)
         elif pressedKeys[-1] == pygame.K_DOWN:
-            y+=blockSize
+            snake.move(0, 1)
             
     
-    win.fill(black)
-    
-    pygame.draw.rect(win, snakeColor, (x, y, blockSize, blockSize))   
+    win.fill(black)    
     drawGrid(win, white)
+    snake.draw()
     
     pygame.display.update()
     pygame.time.delay(70)
